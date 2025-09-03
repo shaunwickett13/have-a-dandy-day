5f19ed9c3d9c412ae11be4bd5aa8374708a58a19
Shaun Wickett <shaunwickett13@gmail.com>
2025-09-02 22:39:58 -0400

    Changes

--- amplify/backend.js
+++ amplify/backend.js
@@ -3,50 +3,42 @@ import { counterApi } from './functions/counter-api/resource';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { Stack } from 'aws-cdk-lib';
-import { Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
-
 /**
  * Backend for Have A Dandy Day - Counter API only
  */
 const backend = defineBackend({
-  counterApi
+    counterApi
 });
-
 // Get the underlying stack from the function
 const functionStack = Stack.of(backend.counterApi.resources.lambda);
-
 // Create DynamoDB table in the same stack as the Lambda function
 // Let CDK generate a unique table name to avoid conflicts
 const counterTable = new Table(functionStack, 'DandyDayCounterTable', {
-  partitionKey: {
-    name: 'id',
-    type: AttributeType.STRING
-  },
-  billingMode: BillingMode.PAY_PER_REQUEST
+    partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+    },
+    billingMode: BillingMode.PAY_PER_REQUEST
 });
-
 // Grant the Lambda function permissions to read/write to the table
 counterTable.grantReadWriteData(backend.counterApi.resources.lambda);
-
 // Pass the table name to the Lambda function as an environment variable
-const lambdaFunction = backend.counterApi.resources.lambda as LambdaFunction;
+const lambdaFunction = backend.counterApi.resources.lambda;
 lambdaFunction.addEnvironment('TABLE_NAME', counterTable.tableName);
-
 // Create REST API in the same stack
 const api = new LambdaRestApi(functionStack, 'DandyDayApi', {
-  handler: backend.counterApi.resources.lambda,
-  proxy: true,
-  restApiName: 'DandyDayApi',
-  defaultCorsPreflightOptions: {
-    allowOrigins: ['*'],
-    allowMethods: ['GET', 'POST', 'OPTIONS'],
-    allowHeaders: ['Content-Type']
-  }
+    handler: backend.counterApi.resources.lambda,
+    proxy: true,
+    restApiName: 'DandyDayApi',
+    defaultCorsPreflightOptions: {
+        allowOrigins: ['*'],
+        allowMethods: ['GET', 'POST', 'OPTIONS'],
+        allowHeaders: ['Content-Type']
+    }
 });
-
 // Add the API URL to the stack outputs
 backend.addOutput({
-  custom: {
-    API_URL: api.url
-  }
+    custom: {
+        API_URL: api.url
+    }
 });
---
+++ amplify/functions/counter-api/resource.js
@@ -0,0 +1,7 @@
+import { defineFunction } from '@aws-amplify/backend';
+export const counterApi = defineFunction({
+    name: 'counter-api',
+    entry: './handler.ts', // Explicitly specify the handler file
+    runtime: 20,
+    timeoutSeconds: 30
+});
